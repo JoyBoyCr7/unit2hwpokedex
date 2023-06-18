a10501d3f1b00a3d0a4c4b464bf66ab8f23afa93
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-06-17 23:53:55 -0500

    can now add a pokemon

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ const app = express()
 const methodOverride = require("method-override")
 const pokemondb = require("./models/pokemon")
 
+const beginning = []
+function firstPekemon (){
+    for (let i = 0; i < 3; i ++){
+        beginning.push(pokemondb[i])
+    }
+    return beginning
+}
 // middleware
+
+//// testing
+console.log(firstPekemon())
+//// testing
+console.log(beginning[0].img)
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(methodOverride("_method"))
@@ -16,21 +28,39 @@ app.use(morgan("dev"))
 const port = process.env.PORT
 
 // index
+
 app.get("/pokemon",(req,res)=>{
-    res.render("index.ejs",{pokemondb})
+    res.render("index.ejs",{beginning})
 })
+
+// New
+app.get("/pokemon/new",(req,res)=>{
+    res.render("new.ejs")
+})
+// delete
+
+// Update
+
+// Create
+app.post("/pokemon/create",(req,res)=>{
+    let name = req.body.name
+    for (let i = 0; i < pokemondb.length; i++){
+        if (name === pokemondb[i].name){
+            beginning.push(pokemondb[i])
+            res.redirect("/pokemon")
+        }  
+    }
+})
+// Edit
+
 
 // SHOW
 app.get('/pokemon/:id', (req, res) => {
     const id = req.params.id
     console.log(id)
-    res.render('show.ejs', {pokemondb,id});
+    res.render('show.ejs', {beginning,id});
     });
 
-// New
-app.get("/pokemon/new",(req,res)=>{
-
-})
 
 app.listen(port,()=>{
     console.log(`live on port ${port}`)
